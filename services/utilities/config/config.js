85e3f93eccd12bf50f8c790f04ae21cdcae450a0
gem-manish <manish.pandey@geminisolutions.com>
2022-08-22 15:23:30 +0530

    Added verifyMSALToken Lambda function

--- services/utilities/config/config.js
+++ services/utilities/config/config.js
@@ -2,7 +2,8 @@ export const urlStore = {
     dev: {
         MONGODB_URI : "mongodb://localhost:27017/orgchart-serverless",
         JWT_SECRET : "anydemosecret",
-        LOG_LEVEL : "debug"
+        LOG_LEVEL : "debug",
+        msalServiceUrl: 'https://graph.microsoft.com/v1.0/me'
     },
     production: {
     },
