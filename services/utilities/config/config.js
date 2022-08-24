16c36f427e3af5abf346e7af93d57d244b9d042b
gem-manish <manish.pandey@geminisolutions.com>
2022-08-24 18:15:26 +0530

    Changed mongoose connection string

--- services/utilities/config/config.js
+++ services/utilities/config/config.js
@@ -1,6 +1,6 @@
 export const urlStore = {
     dev: {
-        MONGODB_URI : "mongodb+srv://manish:pNLBpBDlEE35sjr4@cluster0.17vcdlq.mongodb.net/?retryWrites=true&w=majority",
+        MONGODB_URI : "mongodb://localhost:27017/orgchart-serverless",
         JWT_SECRET : "anydemosecret",
         LOG_LEVEL : "debug",
         msalServiceUrl: 'https://graph.microsoft.com/v1.0/me',
