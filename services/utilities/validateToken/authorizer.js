2d3139d3bc226ce464eb2d370a8d133b78cd121d
gem-manish <manish.pandey@geminisolutions.com>
2022-08-24 16:30:12 +0530

    Added Source Based Access

--- services/utilities/validateToken/authorizer.js
+++ services/utilities/validateToken/authorizer.js
@@ -1,7 +1,7 @@
 import * as jwt from "jsonwebtoken";
 import { getUserRole } from "../getRole";
 import { urlStore } from "../config/config";
-export const authorizer = async (event) => {
+export const accessAllowed = async (event) => {
   try {
     const key =  urlStore[process.env.stage].JWT_SECRET;
     let decode = jwt.verify(event.token, key);
@@ -10,10 +10,37 @@ export const authorizer = async (event) => {
     if (allowedFor.includes(userRole)) {
       return "allowed";
     } else {
-      return {
-        Message:
-          "User is not authorized to access this resource with an explicit deny",
-      };
+      return "denied";
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const accessDeniedToSource = async(event) => {
+  try {
+    const key =  urlStore[process.env.stage].JWT_SECRET;
+    let decode = jwt.verify(event.token, key);
+    let source = event.eventObject.path.source || event.eventObject.pathParameters.source;
+    let role = await getUserRole(decode.email);
+    if ((event.deniedSources).includes(source)) {
+      if((event.deniedRoles).includes(role)){
+        return "denied";
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const accessAllowedToSource = async (event) => {
+  try {
+    const key =  urlStore[process.env.stage].JWT_SECRET;
+    let decode = jwt.verify(event.token, key);
+    let source = event.eventObject.path.source || event.eventObject.pathParameters.source;
+    let role = await getUserRole(decode.email);
+    if (event.allowedSources.includes(source)) {
+      if(event.allowedRoles.includes(role)){
+        return "allowed";
+      }
     }
   } catch (err) {
     console.log(err);
