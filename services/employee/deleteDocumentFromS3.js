6bb7258e27e90274f36409714a2dc4635e93a1b5
gem-manish <manish.pandey@geminisolutions.com>
2022-08-26 15:32:29 +0530

    Handler Function For  deleteDocumentFromS3 API

---
+++ services/employee/deleteDocumentFromS3.js
@@ -0,0 +1,50 @@
+import { internalServer, forbiddenRequest, badRequest } from "../utilities/response/index";
+import { accessAllowed } from "../utilities/validateToken/authorizer";
+import { getUserToken } from "../utilities/validateToken/getUserToken";
+import { EmployeeModel } from "../utilities/dbModels/employee";
+import * as mongoose from 'mongoose';
+import { s3SignedUrlDocuments } from "../utilities/s3SignedUrl/s3SignedUrlDocuments";
+export const deleteDocumentFromS3 = async (event) => {
+    try {
+        console.log(event);
+        let userToken = null;
+        userToken = getUserToken(event);
+        let authQuery = {
+            token: userToken,
+            allowedFor: ['management_su', 'hr_su']
+        };
+        let auth = await accessAllowed(authQuery);
+        if (auth !== "allowed") {
+            return forbiddenRequest("❌❌User is not allowed to access the data");
+        }
+        if (!event.body.documentId) {
+            return badRequest("🤔🤔 Missing body parameters");
+        }
+        const employeeID = event.path.id;
+        const { documentId } = event.body;
+        const empObject = await getEmployeeObjectForS3DocumentDelete(documentId);
+        await s3SignedUrlDocuments('deleteDoc', empObject[0]);
+        const finalResponse = await deleteEmployeeDocument( employeeID, documentId );
+        // console.log("Deleted from S3", finalResponse);
+        return finalResponse;
+    } catch (err) {
+        console.log(err);
+        throw internalServer(`Error in DB `, err);
+    }
+};
+async function getEmployeeObjectForS3DocumentDelete(_id) {
+    const docId = mongoose.Types.ObjectId(_id);
+    let employeeObject = await EmployeeModel.aggregate([{$unwind: "$Documents"},{$match:{'Documents._id':docId}}]);
+    return employeeObject;
+}
+async function deleteEmployeeDocument(_id, documentId) {
+    await EmployeeModel.updateOne(
+      { _id },
+      { $pull: { Documents: { _id: documentId } } }
+    );
+    return {
+      success: true,
+      data: { _id, documentId },
+      message: 'Document removed',
+    };
+}
