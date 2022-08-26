2a2ad791ec9b67736f13ac12c523579104a531d2
gem-manish <manish.pandey@geminisolutions.com>
2022-08-26 18:48:31 +0530

    Added rootDirectory

--- services/utilities/config/config.js
+++ services/utilities/config/config.js
@@ -1,6 +1,8 @@
+import * as path from 'path';
 export const urlStore = {
     dev: {
         MONGODB_URI : "mongodb://localhost:27017/orgchart-serverless",
+        rootDirectory: path.join(__dirname, '..', '..'),
         JWT_SECRET : "anydemosecret",
         LOG_LEVEL : "debug",
         msalServiceUrl: 'https://graph.microsoft.com/v1.0/me',
