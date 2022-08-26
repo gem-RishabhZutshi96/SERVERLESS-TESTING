d34a44ac0b12799bd7c5749ce7a83a68215f9121
gem-manish <manish.pandey@geminisolutions.com>
2022-08-26 18:51:53 +0530

    Export storagePath, dateFormat Function

---
+++ services/utilities/misc/utils.js
@@ -0,0 +1,42 @@
+import { urlStore } from "../config/config";
+import * as path from 'path';
+export const storagePath = (...relative) => {
+    return path.join(urlStore[process.env.stage].rootDirectory, 'storage', ...relative);
+};
+export const dateFormat = (date, format, separator) => {
+    if (isDate(date)) {
+        let dateString;
+        switch (format) {
+            case 'dmy':
+                dateString =
+                    date.getDate() +
+                    separator +
+                    `${date.getMonth() + 1}` +
+                    separator +
+                    date.getFullYear();
+                break;
+            case 'mdy':
+                dateString =
+                    `${date.getMonth() + 1}` +
+                    separator +
+                    date.getDate() +
+                    separator +
+                    date.getFullYear();
+                break;
+            case 'ymd':
+                dateString =
+                    date.getFullYear() +
+                    separator +
+                    `${date.getMonth() + 1}` +
+                    separator +
+                    date.getDate();
+                break;
+        }
+        return dateString;
+    }
+    return date;
+};
+function isDate(input) {
+    if (Object.prototype.toString.call(input) === '[object Date]') return true;
+    return false;
+}
