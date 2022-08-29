a5fc61cb89ec969b6ac57fef874b8e762efedae0
gem-manish <manish.pandey@geminisolutions.com>
2022-08-29 18:22:16 +0530

    Export Function For picture/:id API

---
+++ services/utilities/misc/updateEmployee.js
@@ -0,0 +1,23 @@
+import { EmployeeModel } from "../dbModels/employee";
+export const updateEmployee = async( _id, newEmployeeData ) => {
+    if(newEmployeeData.Cost != undefined) {
+        newEmployeeData.Cost = Buffer.from(`${newEmployeeData.Cost}`).toString('base64');
+    }
+    // check view and update
+    if (newEmployeeData['view'] == 'gemini') {
+        newEmployeeData['OfficialParentID'] = newEmployeeData['HierarchyType']
+    }
+    else if (newEmployeeData['view'] == 'team') {
+        newEmployeeData['ParentTeamID'] = newEmployeeData['HierarchyType']
+    }
+    else if (newEmployeeData['view'] == 'service') {
+        newEmployeeData['ParentServiceManagerID'] = newEmployeeData['HierarchyType']
+
+    }
+    else if (newEmployeeData['view'] == 'project') {
+        newEmployeeData['ProjectWiseViewID'] = newEmployeeData['HierarchyType']
+
+    }
+    await EmployeeModel.updateOne({ _id }, { $set: newEmployeeData });
+    return { success: true, data: { _id }, message: 'Employee updated' };
+};
