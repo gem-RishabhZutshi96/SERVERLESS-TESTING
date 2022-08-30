67e66d0563b11ef452eb29cd46afd81b7d0b6194
gem-manish <manish.pandey@geminisolutions.com>
2022-08-30 15:31:57 +0530

    Handler Function for  employee/update/{id} API

---
+++ services/employee/updateEmployeeDetailsDb.js
@@ -0,0 +1,25 @@
+import { internalServer, forbiddenRequest } from "../utilities/response/index";
+import { accessAllowed } from "../utilities/validateToken/authorizer";
+import { getUserToken } from "../utilities/validateToken/getUserToken";
+import { updateEmployee } from "../utilities/misc/updateEmployee";
+export const updateEmployeeDetailsDb = async (event) => {
+    try {
+        let userToken = null;
+        userToken = getUserToken(event);
+        let authQuery = {
+            token: userToken,
+            allowedFor: ['management_su', 'hr_su']
+        };
+        let auth = await accessAllowed(authQuery);
+        if (auth !== "allowed") {
+            return forbiddenRequest("❌❌User is not allowed to access the data");
+        }
+        const employeeID = event.path.id;
+        const newEmployeeData = event.body;
+        const response = await updateEmployee(employeeID, newEmployeeData);
+        return response;
+    } catch (err) {
+        console.log(err);
+        throw internalServer(`Error in DB`, err);
+    }
+};
