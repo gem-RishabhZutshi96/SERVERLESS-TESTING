7d30edcb0a4a03e2c959d46f4b2156372f237c9b
gem-manish <manish.pandey@geminisolutions.com>
2022-11-09 10:45:13 +0530

    Handler function for misc/fetch/employeeRoles

---
+++ services/misc/fetchEmployeeRoles.js
@@ -0,0 +1,27 @@
+import { makeDBConnection } from "../utilities/db/database";
+import {RoleModel} from "../utilities/dbModels/role";
+import { internalServer } from "../utilities/response/index";
+import {validateToken} from "../utilities/validateToken/validateToken";
+export const fetchEmployeeRoles = async(event) => {
+    try{
+      await makeDBConnection();
+      let auth = validateToken(event);
+      if(auth.email){
+        const roleObj = await RoleModel.find();
+        if(roleObj.length < 1){
+          return {
+            success: false,
+            message: 'Roles Not Found',
+          };
+        }
+        return {
+          data: roleObj,
+          success: true,
+          message: 'Roles Fetched Successfully',
+        };
+      } else return auth;
+    } catch(err) {
+      console.log(err);
+      throw internalServer(`Error in DB `, err);
+    }
+};
