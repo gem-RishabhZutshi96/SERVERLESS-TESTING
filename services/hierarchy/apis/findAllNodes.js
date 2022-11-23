5dd544bb4700d7c9f498f602866cc41e02aad0fa
gem-manish <manish.pandey@geminisolutions.com>
2022-11-23 13:55:11 +0530

    Updated Neo4J DB connection

--- services/hierarchy/apis/findAllNodes.js
+++ services/hierarchy/apis/findAllNodes.js
@@ -1,15 +1,21 @@
 import { makeNeo4jDBConnection } from "../../utilities/db/neo4jdatabase";
 import { devLogger, errorLogger } from "../utils/log-helper";
 import { internalServer } from "../../utilities/response";
+import { urlStore } from '../../utilities/config/config';
 export const findAllNodes = async(event) => {
   try {
+      const { database } = urlStore[process.env.stage].NEO4J;
       devLogger("findAllNodes", event, "event");
-      let session = await makeNeo4jDBConnection();
-      const result = await session.run(`MATCH (U) RETURN U`);
+      let driver = await makeNeo4jDBConnection();
+      let session = driver.session({ database });
+      const result = await session.run(`
+        MATCH N
+        RETURN N`
+      );
       let response = {
           success: true,
           message: "Nodes fetched successfully",
-          data: result.records.map(i => i.get('U').properties)
+          data: result.records.map(i => i.get('N').properties)
       };
       return response;
     } catch(err) {
