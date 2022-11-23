41e74f1fb3a2cac548d6ed5ec511db8d462da70e
gem-manish <manish.pandey@geminisolutions.com>
2022-11-23 13:55:38 +0530

    Handler Function for findEmployeeHierarchy API

---
+++ services/hierarchy/apis/findEmployeeHierarchy.js
@@ -0,0 +1,44 @@
+import { makeNeo4jDBConnection } from "../../utilities/db/neo4jdatabase";
+import { badRequest } from "../../utilities/response/index";
+import { devLogger, errorLogger } from "../utils/log-helper";
+import { internalServer } from "../../utilities/response";
+import { urlStore } from '../../utilities/config/config';
+export const findEmployeeHierarchy = async(event) => {
+  try {
+      const { database } = urlStore[process.env.stage].NEO4J;
+      devLogger("findEmployeeHierarchy", event, "event");
+      if(!(event.body.relation || event.body.empName)){
+        return badRequest("🤔🤔 Missing body parameters");
+      } else {
+        const { relation, empName } = event.body;
+        let driver = await makeNeo4jDBConnection();
+        let session = driver.session({ database });
+        if(!empName){
+          const result = await session.run(`
+          MATCH (N1:EMPLOYEE)-[:${relation}]->(N2:EMPLOYEE)
+          RETURN N1, N2
+        `);
+        let response = {
+          success: true,
+          message: "Nodes fetched successfully",
+          data: result.records.map(i => i.get('N1').properties)
+        };
+        return response;
+        } else {
+          const result = await session.run(`
+          MATCH (N1)-[:${relation}]->(N2:EMPLOYEE {Name:"${empName}"})
+          RETURN N1`);
+          let response = {
+            success: true,
+            message: "Nodes fetched successfully",
+            data: result.records.map(i => i.get('N1').properties)
+          };
+          return response;
+        }
+      }
+    } catch(err) {
+      errorLogger("findEmployeeHierarchy", err, "Error db call");
+      console.log(err);
+      throw internalServer(`Error in DB `, err);
+  }
+};
