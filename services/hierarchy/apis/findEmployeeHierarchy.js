caeb881fe973b8a20b5a08f73b987550cc692fce
gem-manish <manish.pandey@geminisolutions.com>
2022-11-24 18:59:42 +0530

    Handler Function for hierarchy  API

--- services/hierarchy/apis/findEmployeeHierarchy.js
+++ services/hierarchy/apis/findEmployeeHierarchy.js
@@ -7,37 +7,24 @@ export const findEmployeeHierarchy = async(event) => {
   try {
       const { database } = urlStore[process.env.stage].NEO4J;
       devLogger("findEmployeeHierarchy", event, "event");
-      if(!(event.body.relation || event.body.empName)){
-        return badRequest("🤔🤔 Missing body parameters");
-      } else {
-        const { relation, empName } = event.body;
+      let source = event.path.source || event.pathParameters.source;
+      if(urlStore[process.env.stage].sourceViews.hasOwnProperty(`${source}`)){
         let driver = await makeNeo4jDBConnection();
         let session = driver.session({ database });
-        if(!empName){
-          const result = await session.run(`
-          MATCH p=(N1:EMPLOYEE)-[:${relation}*]->(N2:EMPLOYEE)
+        let view = urlStore[process.env.stage].sourceViews[`${source}`];
+        const result = await session.run(`
+          MATCH p=(N1:EMPLOYEE {officialID:"${view.id}"})<-[:${view.relation}*]-(N2:EMPLOYEE)
           WITH COLLECT(p) AS ps
           CALL apoc.convert.toTree(ps) yield value
-          RETURN value;`);
+          RETURN apoc.convert.toJson(value);`);
         let response = {
           success: true,
-          message: "Nodes fetched successfully",
+          message: "Hierarchy fetched successfully",
           data: result.records.map(i => i.get('value'))
         };
         return response;
-        } else {
-          const result = await session.run(`
-          MATCH p=(N1)-[:${relation}*]->(N2:EMPLOYEE {Name:"${empName}*"})
-          WITH COLLECT(p) AS ps
-          CALL apoc.convert.toTree(ps) yield value
-          RETURN value;`);
-          let response = {
-            success: true,
-            message: "Nodes fetched successfully",
-            data: result.records.map(i => i.get('value'))
-          };
-          return response;
-        }
+      } else {
+        return badRequest("Invalid path parameters");
       }
     } catch(err) {
       errorLogger("findEmployeeHierarchy", err, "Error db call");
