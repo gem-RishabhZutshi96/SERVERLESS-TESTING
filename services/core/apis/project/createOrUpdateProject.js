2eaaad1e74351c5e362d2dd6477f5ab29f3fa852
gem-manish <manish.pandey@geminisolutions.com>
2022-12-02 17:02:59 +0530

    Added Handler Functions for Project APIs

---
+++ services/core/apis/project/createOrUpdateProject.js
@@ -0,0 +1,39 @@
+import { makeDBConnection } from "../../utilities/db/mongo";
+import {RoleModel} from "../../utilities/dbModels/role";
+import { internalServer } from "../../utilities/response/index";
+import { accessAllowed } from "../../utilities/validateToken/authorizer";
+import { getUserToken } from "../../utilities/validateToken/getUserToken";
+import { devLogger, errorLogger } from "../utils/log-helper";
+export const createOrUpdateProject = async(event) => {
+    try{
+      devLogger("createOrUpdateRole", event, "event");
+      let userToken =null;
+      await makeDBConnection();
+      userToken = getUserToken(event);
+      let authQuery={
+        token: userToken,
+        allowedFor:['management_su']
+      };
+      let auth= await accessAllowed(authQuery);
+      if(auth!=="allowed"){
+        return auth;
+      }
+      const filter = { email: event.body.email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          role: event.body.role
+        },
+      };
+      await RoleModel.updateOne(filter, updateDoc, options);
+      let response = {
+        success:true,
+        email: event.body.email,
+        role: event.body.role
+      };
+      return response;
+    } catch(err) {
+      errorLogger("createOrUpdateProject", err, "Error db call");
+      throw internalServer(`Error in DB `, err);
+    }
+};
