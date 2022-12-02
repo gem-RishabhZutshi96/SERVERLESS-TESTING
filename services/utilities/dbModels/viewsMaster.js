52bb36baeeddaf097e405bc2e20d10c87da8461f
gem-manish <manish.pandey@geminisolutions.com>
2022-12-02 09:08:54 +0530

    Added Model for Views Master Table

---
+++ services/utilities/dbModels/viewsMaster.js
@@ -0,0 +1,25 @@
+import { Schema as _Schema,  model } from 'mongoose';
+var Schema = _Schema;
+const viewsMaster = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+    },
+    viewId: {
+      type: String,
+      unique: true,
+    },
+    relationName: {
+        type: String,
+    }
+  },
+  {
+    toJSON: {
+      transform: function(doc, ret, options) {
+        delete ret.__v;
+      },
+    },
+  }
+);
+export const viewModel = model('viewsMaster', viewsMaster);
