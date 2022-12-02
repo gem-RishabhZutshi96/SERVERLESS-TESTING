0eb25dc0061aedcf26e3935fb9113ed215807804
gem-manish <manish.pandey@geminisolutions.com>
2022-12-02 09:52:59 +0530

    Export function to fetch data from MIS

---
+++ services/core/externalCall/getDataService.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import {
+    urlStore
+} from "../../utilities/config/config";
+import {
+    internalServer
+  } from '../../utilities/response/index';
+import {
+    errorLogger,
+    devLogger
+  } from '../utils/log-helper';
+const url = urlStore[process.env.stage].misApi.uri;
+export const getDataService = async () => {
+    try{
+      devLogger('Mis API Hit', "", 'event');
+      let result = await axios.get(url);
+      return result.data; 
+    } catch(err) {
+        errorLogger('Mis API Hit', err, 'Error in Getting Data from MIS');
+        throw internalServer(`Error in MIS API`, err);
+    }
+};
