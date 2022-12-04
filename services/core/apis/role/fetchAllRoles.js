613e8f11d941e6a417f145372b6da280cab4f499
gem-manish <manish.pandey@geminisolutions.com>
2022-12-04 12:25:45 +0530

    Updated Response for Fetch Roles API

--- services/core/apis/role/fetchAllRoles.js
+++ services/core/apis/role/fetchAllRoles.js
@@ -1,6 +1,6 @@
 import { makeDBConnection } from "../../../utilities/db/mongo";
 import { roleMasterModel } from "../../../utilities/dbModels/roleMaster";
-import { internalServer } from "../../../utilities/response/index";
+import { internalServer, successResponse, failResponse } from "../../../utilities/response/index";
 import { devLogger, errorLogger } from "../../utils/log-helper";
 export const fetchAllRoles = async(event) => {
     try{
@@ -8,16 +8,9 @@ export const fetchAllRoles = async(event) => {
         await makeDBConnection();
         const obj = await roleMasterModel.find();
         if(obj.length < 1){
-            return {
-                success: false,
-                message: 'Roles Not Found',
-            };
+            return failResponse('Roles Not Found');
         } 
-        return {
-        data: obj,
-        success: true,
-        message: 'Roles Fetched Successfully',
-        };
+        return successResponse('Roles Fetched Successfully', obj);
     } catch(err) {
       errorLogger("fetchAllRoles", err, "Error db call");
       throw internalServer(`Error in DB `, err);
