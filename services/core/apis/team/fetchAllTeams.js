58f56637542e56c9ed5169c94aba50bf484deae8
gem-manish <manish.pandey@geminisolutions.com>
2022-12-04 13:03:33 +0530

    Updated Handler Function to Fetch Teams

---
+++ services/core/apis/team/fetchAllTeams.js
@@ -0,0 +1,18 @@
+import { makeDBConnection } from "../../../utilities/db/mongo";
+import { teamModel } from "../../../utilities/dbModels/team";
+import { internalServer, successResponse, failResponse } from "../../../utilities/response/index";
+import { devLogger, errorLogger } from "../../utils/log-helper";
+export const fetchAllTeams = async(event) => {
+    try{
+        devLogger("fetchAllTeams", event, "event");
+        await makeDBConnection();
+        const obj = await teamModel.find();
+        if(obj.length < 1){
+            return failResponse('Teams Not Found');
+        } 
+        return successResponse('Teams Fetched Successfully', obj);
+    } catch(err) {
+      errorLogger("fetchAllTeams", err, "Error db call");
+      throw internalServer(`Error in DB `, err);
+    }
+};
