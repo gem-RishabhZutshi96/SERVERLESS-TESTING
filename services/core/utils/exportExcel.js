3b9aea2e41eb1eab022c5a77eee59ad3757785d8
gem-manish <manish.pandey@geminisolutions.com>
2022-12-05 16:23:22 +0530

    Utility functions for exporting excel data of master tables

--- services/core/utils/exportExcel.js
+++ services/core/utils/exportExcel.js
@@ -1,8 +1,12 @@
 import { employeeMasterModel } from "../../utilities/dbModels/employeeMaster";
+import { projectModel } from "../../utilities/dbModels/project";
+import { teamModel } from "../../utilities/dbModels/team";
+import * as json2xls from 'json2xls';
+import { errorLogger } from "./log-helper";
 export const exportExcelDataEmpMaster = async (fileName) => {
     try {
         const employees = await employeeMasterModel.find().lean();
-        const xls = json2xls(employees.map(emp => getEmpMasterJSONForExcel(emp)));
+        const xls = json2xls(employees.map(emp => getEmpMasterJSON(emp)));
         const buffer = Buffer.from(xls, 'binary');
         await uploadToS3({
             Bucket: dataStore[process.env.stage].s3Params.sowBucket,
@@ -17,13 +21,61 @@ export const exportExcelDataEmpMaster = async (fileName) => {
         });
         return downloadURL;
     } catch (e) {
-        console.log('Error in exporting employees excel');
-        console.log(e);
+        errorLogger('Error in exporting employees master table excel:::::', e);
         throw e;
     }
 };
 
-function getEmpMasterJSONForExcel(emp) {
+
+export const exportExcelDataProjectMaster = async (fileName) => {
+    try {
+        const employees = await projectModel.find().lean();
+        const xls = json2xls(employees.map(emp => getProjectMasterJSON(emp)));
+        const buffer = Buffer.from(xls, 'binary');
+        await uploadToS3({
+            Bucket: dataStore[process.env.stage].s3Params.sowBucket,
+            Key: fileName,
+            ContentType: 'application/vnd.ms-excel',
+            Body: buffer
+        });
+        let downloadURL = await getS3SignedUrl({
+            Bucket: dataStore[process.env.stage].s3Params.sowBucket,
+            Key: fileName,
+            Expires: 3600
+        });
+        return downloadURL;
+    } catch (e) {
+        errorLogger('Error in exporting project master table excel::::', e);
+        throw e;
+    }
+};
+
+
+export const exportExcelDataTeamMaster = async (fileName) => {
+    try {
+        const employees = await teamModel.find().lean();
+        const xls = json2xls(employees.map(emp => getTeamMasterJSON(emp)));
+        const buffer = Buffer.from(xls, 'binary');
+        await uploadToS3({
+            Bucket: dataStore[process.env.stage].s3Params.sowBucket,
+            Key: fileName,
+            ContentType: 'application/vnd.ms-excel',
+            Body: buffer
+        });
+        let downloadURL = await getS3SignedUrl({
+            Bucket: dataStore[process.env.stage].s3Params.sowBucket,
+            Key: fileName,
+            Expires: 3600
+        });
+        return downloadURL;
+    } catch (e) {
+        errorLogger('Error in exporting team master table excel::::', e);
+        throw e;
+    }
+};
+
+
+function getEmpMasterJSON(emp) {
     const data = {
         'Email Id': emp.EmailId,
         'Employee Code': emp.EmployeeCode,
@@ -44,6 +96,31 @@ function getEmpMasterJSONForExcel(emp) {
     );
 }
 
+
+function getProjectMasterJSON(prj) {
+    const data = {
+        'Name': prj.name,
+        'Project Id': prj.projectId,
+        'Description': prj.description,
+    };
+    return JSON.parse(
+        JSON.stringify(data, (key, value) => (value == undefined ? '' : value))
+    );
+}
+
+
+function getTeamMasterJSON(team) {
+    const data = {
+        'Name': team.name,
+        'Team Id': team.teamId,
+        'Description': team.description,
+    };
+    return JSON.parse(
+        JSON.stringify(data, (key, value) => (value == undefined ? '' : value))
+    );
+}
+
+
 async function uploadToS3(s3Data) {
     console.log("---- UPLODAING TO S3 ----",JSON.stringify(`${s3Data.Bucket} ${s3Data.Key}`, null, 2));
     try {
@@ -53,6 +130,8 @@ async function uploadToS3(s3Data) {
       return error;
     }
 }
+
+
 async function getS3SignedUrl(params) {
     console.log("---- GETTING SIGNED URL FROM S3 ----", JSON.stringify(params, null, 2));
     try {
