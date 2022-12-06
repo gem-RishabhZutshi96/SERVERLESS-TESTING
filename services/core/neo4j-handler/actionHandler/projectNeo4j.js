1349597645ddc16329ea3a7ecb331fc564400cef
gem-manish <manish.pandey@geminisolutions.com>
2022-12-06 16:01:45 +0530

    Added required imports and script to update project node in neo4j

--- services/core/neo4j-handler/actionHandler/projectNeo4j.js
+++ services/core/neo4j-handler/actionHandler/projectNeo4j.js
@@ -2,34 +2,34 @@ import {
     makeNeo4jDBConnection
 } from "../../../utilities/db/neo4j";
 import cryptoRandomString from 'crypto-random-string';
-export const addProjectNeo4j = async (event) => {
+import { parameterStore } from "../../../utilities/config/commonData";
+import cryptoRandomString from 'crypto-random-string';
+import { internalServer, successResponse } from "../../../utilities/response";
+export const createOrUpdateProjectNeo4j = async (event) => {
     try {
-      devLogger("addProjectNeo4j", event, "event");
+      devLogger("createOrUpdateProjectNeo4j", event, "event");
       const { database } = parameterStore[process.env.stage].NEO4J;
       let driver = await makeNeo4jDBConnection();
       let session = driver.session({ database });
       const exist = await session.run(
         `OPTIONAL MATCH (n:${event.label} {${event.filter}:"${event.node.id}"})
         RETURN n IS NOT NULL AS Predicate`);
-      if(!exist){
+      if(!exist) {
         const createNode = await session.run(`
           CREATE
           (${cryptoRandomString({length: 5, type: 'base64'})}:${event.label}{projectId:"${event.node.id}", name:"${event.node.name}", desciption:"${event.node.description}"})
         `);
+        return successResponse('Node Created Successfully');
       } else {
-        return {
-          statusCode: "[404]",
-          message: "Tax Saving Options Data Not Found",
-          data: result,
-        };
+        await session.run(`
+          MERGE
+          (${cryptoRandomString({length: 5, type: 'base64'})}:${event.label}{projectId:"${event.node.id}", name:"${event.node.name}", desciption:"${event.node.description}"})
+        `);
+        return successResponse('Node Updated Successfully');
       }
     } catch (err) {
-      errorLogger(
-        "addProjectNeo4j",
-        err,
-        "Error in DB aggregate query for collection -> TaxSavingOption"
-      );
-      throw internalServer(`Error in Fetching Tax Saving Options Data `, err);
+      errorLogger("createOrUpdateProjectNeo4j::::", err);
+      throw internalServer(`Error in Creating or Updating Node::::`, err);
     }
 };
 
@@ -41,38 +41,11 @@ export const deleteProjectNeo4j = async (event) => {
     let session = driver.session({ database });
     const deleteNode = await session.run(
       `MATCH (n:${event.label} {projectId:"${event.node.id}"})
-      DETACH DELETE n`);
+      DETACH DELETE n
+    `);
+    return successResponse('Node Deleted Successfully');
   } catch (err) {
-    errorLogger(
-      "deleteProjectNeo4j",
-      err,
-      "Error in DB aggregate query for collection -> TaxSavingOption"
-    );
-    throw internalServer(`Error in Fetching Tax Saving Options Data `, err);
-  }
-};
-
-export const updateProjectNeo4j = async (event) => {
-  try {
-    devLogger("getData", event, "event");
-    const { database } = parameterStore[process.env.stage].NEO4J;
-    let driver = await makeNeo4jDBConnection();
-    let session = driver.session({ database });
-    const exist = await session.run(
-      `OPTIONAL MATCH (n:${event.label} {${event.filter}:"${event.node.id}"})
-      RETURN n IS NOT NULL AS Predicate`);
-    if(exist){
-      const createNode = await session.run(`
-        CREATE
-        (${cryptoRandomString({length: 5, type: 'base64'})}:${event.label}{projectId:"${event.node.id}", name:"${event.node.name}", desciption:"${event.node.description}"})
-      `);
-    } 
-  } catch (err) {
-    errorLogger(
-      "getData",
-      err,
-      "Error in DB aggregate query for collection -> TaxSavingOption"
-    );
-    throw internalServer(`Error in Fetching Tax Saving Options Data `, err);
+    errorLogger("deleteProjectNeo4j::::",err);
+    throw internalServer(`Error in Deleting Node::::`, err);
   }
 };
