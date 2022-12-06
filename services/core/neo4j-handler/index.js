a332a4502bbbf9c938caef561e6fb930909b6ac3
gem-manish <manish.pandey@geminisolutions.com>
2022-12-06 16:00:12 +0530

    Added switch cases to process events

--- services/core/neo4j-handler/index.js
+++ services/core/neo4j-handler/index.js
@@ -2,6 +2,18 @@ import {
     errorLogger,
     infoLogger
 } from "../utils/log-helper";
+import {
+    createOrUpdateEmpNeo4j,
+    deleteEmpNeo4j
+} from "./actionHandler/empNeo4j"
+import { 
+    createOrUpdateProjectNeo4j,
+    deleteProjectNeo4j
+} from "./actionHandler/projectNeo4j";
+import { 
+    createOrUpdateTeamNeo4j,
+    deleteTeamNeo4j
+} from "./actionHandler/teamNeo4j";
 export async function main(event) {
     try {
         let result = await processEvent(event);
@@ -18,7 +30,17 @@ const processEvent = async (event) => {
         "process event called with event"
     );
     switch (event.actionType) {
-        case "addProjectNeo4j":
-            return addProjectNeo4j(event);
+        case "createOrUpdateProjectNeo4j":
+            return createOrUpdateProjectNeo4j(event);
+        case "deleteProjectNeo4j":
+            return deleteProjectNeo4j(event);
+        case "createOrUpdateTeamNeo4j":
+            return createOrUpdateTeamNeo4j(event);
+        case "deleteTeamNeo4j":
+            return deleteTeamNeo4j(event);
+        case "createOrUpdateEmpNeo4j":
+            return createOrUpdateEmpNeo4j(event);
+        case "deleteEmpNeo4j":
+            return deleteEmpNeo4j(event);
     }
 };
