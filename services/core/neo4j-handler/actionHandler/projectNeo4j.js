d499098cfd13321b8aa2fe415d90faca548731a5
gem-manish <manish.pandey@geminisolutions.com>
2022-12-06 17:06:27 +0530

    Added filter and label for project node queries

--- services/core/neo4j-handler/actionHandler/projectNeo4j.js
+++ services/core/neo4j-handler/actionHandler/projectNeo4j.js
@@ -12,18 +12,18 @@ export const createOrUpdateProjectNeo4j = async (event) => {
       let driver = await makeNeo4jDBConnection();
       let session = driver.session({ database });
       const exist = await session.run(
-        `OPTIONAL MATCH (n:${event.label} {${event.filter}:"${event.node.id}"})
+        `OPTIONAL MATCH (n:PROJECT {projectId:"${event.node.id}"})
         RETURN n IS NOT NULL AS Predicate`);
       if(!exist) {
         const createNode = await session.run(`
           CREATE
-          (${cryptoRandomString({length: 5, type: 'base64'})}:${event.label}{projectId:"${event.node.id}", name:"${event.node.name}", desciption:"${event.node.description}"})
+          (${cryptoRandomString({length: 5, type: 'base64'})}:PROJECT{projectId:"${event.node.id}", name:"${event.node.name}", desciption:"${event.node.description}"})
         `);
         return successResponse('Node Created Successfully');
       } else {
         await session.run(`
           MERGE
-          (${cryptoRandomString({length: 5, type: 'base64'})}:${event.label}{projectId:"${event.node.id}", name:"${event.node.name}", desciption:"${event.node.description}"})
+          (${cryptoRandomString({length: 5, type: 'base64'})}:PROJECT{projectId:"${event.node.id}", name:"${event.node.name}", desciption:"${event.node.description}"})
         `);
         return successResponse('Node Updated Successfully');
       }
@@ -40,7 +40,7 @@ export const deleteProjectNeo4j = async (event) => {
     let driver = await makeNeo4jDBConnection();
     let session = driver.session({ database });
     const deleteNode = await session.run(
-      `MATCH (n:${event.label} {projectId:"${event.node.id}"})
+      `MATCH (n:PROJECT {projectId:"${event.node.id}"})
       DETACH DELETE n
     `);
     return successResponse('Node Deleted Successfully');
