79cb112efd8ea37706750290c5aa4ff037b93dc5
gem-manish <manish.pandey@geminisolutions.com>
2022-12-07 15:19:18 +0530

    Script to add nodes in neo4j

--- services/core/neo4j-handler/actionHandler/empNeo4j.js
+++ services/core/neo4j-handler/actionHandler/empNeo4j.js
@@ -2,32 +2,40 @@ import {
     makeNeo4jDBConnection
 } from "../../../utilities/db/neo4j";
 import { parameterStore } from "../../../utilities/config/commonData";
-import cryptoRandomString from 'crypto-random-string';
+//import cryptoRandomString from 'crypto-random-string';
 import { internalServer, successResponse } from "../../../utilities/response";
+import { devLogger, errorLogger } from "../../utils/log-helper";
 export const createOrUpdateEmpNeo4j = async (event) => {
     try {
       devLogger("createOrUpdateEmpNeo4j", event, "event");
       const { database } = parameterStore[process.env.stage].NEO4J;
       let driver = await makeNeo4jDBConnection();
       let session = driver.session({ database });
-      const exist = await session.run(
-        `OPTIONAL MATCH (n:EMPLOYEE {EmployeeCode:"${event.node.EmployeeCode}"})
-        RETURN n IS NOT NULL AS Predicate`);
-      if(!exist) {
-        await session.run(`
-          CREATE
-          (${cryptoRandomString({length: 5, type: 'base64'})}:EMPLOYEE{EmployeeCode:"${event.node.EmployeeCode}", EmployeeName:"${event.node.EmployeeName}", Designation:"${event.node.Designation}, ImagePath:"${event.node.ImagePath}"})
-        `);
-        return successResponse('Node Created Successfully');
-      } else {
-        await session.run(`
-          MERGE
-          (${cryptoRandomString({length: 5, type: 'base64'})}:EMPLOYEE{EmployeeCode:"${event.node.EmployeeCode}", EmployeeName:"${event.node.EmployeeName}", Designation:"${event.node.Designation}, ImagePath:"${event.node.ImagePath}"})
-        `);
-        return successResponse('Node Updated Successfully');
-      }
+      // const exist = await session.run(
+      //   `OPTIONAL MATCH (n:EMPLOYEE {EmployeeCode:"${event.node.EmployeeCode}"})
+      //   RETURN n IS NOT NULL AS Predicate`);
+      // if(!exist) {
+      //   await session.run(`
+      //      MERGE
+      //      (${cryptoRandomString({length: 5, type: 'base64'})}:EMPLOYEE{EmployeeCode:"${event.node.EmployeeCode}", EmployeeName:"${event.node.EmployeeName}", Designation:"${event.node.Designation}", ImagePath:"${event.node.ImagePath}"})
+      //     `);
+      //   return successResponse('Node Created Successfully');
+      // } else {
+      //   await session.run(`
+      //     MERGE
+      //     (${cryptoRandomString({length: 5, type: 'base64'})}:EMPLOYEE{EmployeeCode:"${event.node.EmployeeCode}", EmployeeName:"${event.node.EmployeeName}", Designation:"${event.node.Designation}", ImagePath:"${event.node.ImagePath}"})
+      //   `);
+      //  return successResponse('Node Updated Successfully');
+      //}
+      await session.run(`
+        CALL apoc.periodic.iterate("CALL apoc.load.json('${event.downloadURL}') YIELD value",
+        "UNWIND value as emp \r\n"+ 
+        "MERGE (emp.nodeId:EMPLOYEE{EmployeeCode:emp.EmployeeCode, EmployeeName:emp.EmployeeName, Designation:emp.Designation, ImagePath:emp.ImagePath, })",
+        {batchSize:10000, iterateList:true});
+      `);
     } catch (err) {
       errorLogger("createOrUpdateEmpNeo4j::::", err);
+      console.log(err);
       throw internalServer(`Error in Creating or Updating Node::::`, err);
     }
 };
@@ -38,7 +46,7 @@ export const deleteEmpNeo4j = async (event) => {
     const { database } = parameterStore[process.env.stage].NEO4J;
     let driver = await makeNeo4jDBConnection();
     let session = driver.session({ database });
-    const deleteNode = await session.run(
+    await session.run(
       `MATCH (n:EMPLOYEE {EmployeeCode:"${event.node.EmployeeCode}"})
       DETACH DELETE n`);
     return successResponse('Node Deleted Successfully');
