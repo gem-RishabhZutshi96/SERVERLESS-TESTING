39cc02747c9324e8b7255ac638f83ceaf8e025cc
gem-manish <manish.pandey@geminisolutions.com>
2022-12-13 13:46:11 +0530

    Added response status codes

--- services/utilities/response/index.js
+++ services/utilities/response/index.js
@@ -1,6 +1,7 @@
 export function successResponse(message, data) {
     let response = {
         success: true,
+        "StatusCode": 200,
         message,
         data
      };
@@ -9,17 +10,20 @@ export function successResponse(message, data) {
 
 export function badRequest(message, data) {
     let response = {
+        success: false,
+        "StatusCode": 400,
         message,
-        data,
-        success: false
+        data
+        
     };
     return response;
 }
 
 export function forbiddenRequest(message) {
     let response = {
+        success: false,
+        "StatusCode": 403,
         message,
-        success: false
     };
     return response;
 }
@@ -27,14 +31,16 @@ export function forbiddenRequest(message) {
 export function internalServer(message, data) {
     let response = {
         success: false,
+        "StatusCode": 500,
         message,
-        data,
+        data
     };
     return response;
 }
-export function failResponse(message) {
+export function failResponse(message, StatusCode) {
     let response = {
         success: false,
+        "StatusCode": StatusCode,
         message
     };
     return response;
