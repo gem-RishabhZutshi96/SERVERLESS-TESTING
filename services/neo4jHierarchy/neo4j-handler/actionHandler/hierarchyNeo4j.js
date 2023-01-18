fdcf0dc1a69263a83fe2338f28a695bff3a78f83
gem-manish <manish.pandey@geminisolutions.com>
2023-01-18 10:52:04 +0530

    Added action handler for fetch Hierarchy case

--- services/neo4jHierarchy/neo4j-handler/actionHandler/hierarchyNeo4j.js
+++ services/neo4jHierarchy/neo4j-handler/actionHandler/hierarchyNeo4j.js
@@ -25,6 +25,54 @@ export const createHierarchyForExcel = async (event) => {
     }
 };
 
+export const fetchHierarchy = async (event) => {
+  try {
+    devLogger("fetchHierarchy", event, "event");
+    const { database } = parameterStore[process.env.stage].NEO4J;
+    let driver = await makeNeo4jDBConnection();
+    let session = driver.session({ database });
+    let view = parameterStore[process.env.stage].sourceViews[`${event.source}`];
+    const tree = await session.executeRead(async tx => {
+    //   const result = await tx.run(`
+    //   MATCH (a) WHERE ANY(k IN ['teamId', 'projectId', 'EmployeeCode'] WHERE toString(a[k]) CONTAINS $rootId)
+    //   CALL {
+    //     MATCH p=(a)<-[:${view.relation}*]-()
+    //         WHERE apoc.coll.duplicates(NODES(p)) = []
+    //     WITH p ORDER BY length(p) DESC
+    //     WITH COLLECT(DISTINCT p) AS ps
+    //     RETURN ps
+    //   }
+    //   WITH ps
+    //   CALL apoc.convert.toTree(ps)
+    //   YIELD value
+    //   RETURN apoc.convert.toJson(value)
+    // `,{rootId: view.rootId});
+    // return result.records.map(record => record.get('apoc.convert.toJson(value)'))[0];
+    const result = await tx.run(`
+      MATCH (a) WHERE ANY(k IN ['teamId', 'projectId', 'EmployeeCode'] WHERE toString(a[k]) CONTAINS $rootId)
+      CALL apoc.path.expandConfig(a, {
+        relationshipFilter: "${view.relation}<",
+          minLevel: 1,
+          maxLevel: -1,
+          uniqueness:'NODE_GLOBAL',
+          bfs:true
+      })
+      YIELD path WHERE all(rel in relationships(path) WHERE rel.isActive)
+      WITH COLLECT(path) as ps
+      CALL apoc.convert.toTree(ps)
+      YIELD value
+      RETURN apoc.convert.toJson(value)
+  `,{rootId: view.rootId});
+  return result.records.map(record => record.get('apoc.convert.toJson(value)'));
+    });
+    const regex = generateRegex(view.relation);
+    const resp =  tree.toString().replace(regex,`"children":`);
+    return successResponse('Hierarchy fetched successfully', resp);
+  } catch (err) {
+    errorLogger("fetchHierarchy::::", err);
+    throw internalServer(`Error in fetching Hierarchy::::`);
+  }
+};
 
 export const updateHierarchy = async (event) => {
   try {
@@ -82,3 +130,7 @@ export const updateHierarchy = async (event) => {
     throw internalServer(`Error in Updating Hierarchy::::`);
   }
 };
+
+function generateRegex(str) {
+  return new RegExp(`"${str}":`,"ig");
+}
