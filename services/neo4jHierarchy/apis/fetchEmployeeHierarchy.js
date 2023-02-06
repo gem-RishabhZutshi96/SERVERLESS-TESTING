69e8c85fb414217865ab308383f41ae522d787c6
gem-manish <manish.pandey@geminisolutions.com>
2023-02-06 16:30:51 +0530

    Modified response for fetch hierarchy API for newly created views

--- services/neo4jHierarchy/apis/fetchEmployeeHierarchy.js
+++ services/neo4jHierarchy/apis/fetchEmployeeHierarchy.js
@@ -10,19 +10,40 @@ export const fetchEmployeeHierarchy = async(event) => {
       let source = event.path.source || event.pathParameters.source;
       await makeDBConnection();
       const sourceViews = await viewModel.find({ 'name': { '$regex': source, '$options': 'i' } });
-      console.log(sourceViews);
       if(sourceViews.length >= 1){
         const resp = await main({
           actionType: 'fetchHierarchy',
           relationName: sourceViews[0].relationName,
           rootId: sourceViews[0].rootId
         });
-        const nodes = flatten([JSON.parse(resp.data)]);
+        resp.data = JSON.parse(resp.data);
+        const nodes = flatten([resp.data]);
+        if(nodes.length <= 1){
+          let obj = {
+            "id": resp.data.id,
+            "type": resp.data.type,
+            "labels": resp.data.labels,
+            "Designation": resp.data.properties.Designation,
+            "ManagerCode": resp.data.properties.ManagerCode,
+            "EmployeeCode": resp.data.properties.EmployeeCode,
+            "ImagePath": resp.data.properties.ImagePath,
+            "EmployeeName": resp.data.properties.EmployeeName
+          };
+          let response = {
+            success: resp.success,
+            message: resp.message,
+            data: {
+              hierarchyData: Object.assign(obj, {"children":[]}),
+              nodeData: flatten([obj])
+            }
+          };
+          return response;
+        }
         let response = {
           success: resp.success,
           message: resp.message,
           data: {
-            hierarchyData: JSON.parse(resp.data),
+            hierarchyData: resp.data,
             nodeData: nodes
           }
         };
