59e54c77e0f9edbce0b7394a813f56162a50d1f5
gem-manish <manish.pandey@geminisolutions.com>
2023-02-06 16:25:25 +0530

    Added changes in lambda functions due to change in authentication function

--- services/misc/apis/createOrUpdateRole.js
+++ services/misc/apis/createOrUpdateRole.js
@@ -15,7 +15,7 @@ export const createOrUpdateRole = async(event) => {
         allowedFor:['management_su']
       };
       let auth= await accessAllowed(authQuery);
-      if(auth!=="allowed"){
+      if( auth.access !=="allowed"){
         return auth;
       }
       const filter = { email: event.body.email };
