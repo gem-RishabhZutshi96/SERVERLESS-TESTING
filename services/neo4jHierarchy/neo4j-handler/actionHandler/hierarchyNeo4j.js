aae47acc5767917c91499e1a9681f088307d0843
gem-manish <manish.pandey@geminisolutions.com>
2023-02-06 16:32:34 +0530

    Return a single node for newly created views in fetch hierarchy API from Neo4J

--- services/neo4jHierarchy/neo4j-handler/actionHandler/hierarchyNeo4j.js
+++ services/neo4jHierarchy/neo4j-handler/actionHandler/hierarchyNeo4j.js
@@ -30,41 +30,60 @@ export const fetchHierarchy = async (event) => {
     const { database } = parameterStore[process.env.stage].NEO4J;
     let driver = await makeNeo4jDBConnection();
     let session = driver.session({ database });
-    const tree = await session.executeRead(async tx => {
-    //   const result = await tx.run(`
-    //   MATCH (a) WHERE ANY(k IN ['teamId', 'projectId', 'EmployeeCode'] WHERE toString(a[k]) CONTAINS $rootId)
-    //   CALL {
-    //     MATCH p=(a)<-[:${view.relation}*]-()
-    //         WHERE apoc.coll.duplicates(NODES(p)) = []
-    //     WITH p ORDER BY length(p) DESC
-    //     WITH COLLECT(DISTINCT p) AS ps
-    //     RETURN ps
-    //   }
-    //   WITH ps
-    //   CALL apoc.convert.toTree(ps)
-    //   YIELD value
-    //   RETURN apoc.convert.toJson(value)
-    // `,{rootId: view.rootId});
-    // return result.records.map(record => record.get('apoc.convert.toJson(value)'))[0];
-    const result = await tx.run(`
-      MATCH (a) WHERE ANY(k IN ['teamId', 'projectId', 'EmployeeCode'] WHERE toString(a[k]) CONTAINS $rootId)
-      CALL apoc.path.expandConfig(a, {
-        relationshipFilter: "${event.relationName}<",
-          minLevel: 1,
-          maxLevel: -1,
-          uniqueness:'NODE_GLOBAL',
-          bfs:true
-      })
-      YIELD path WHERE all(rel in relationships(path) WHERE rel.isActive)
-      WITH COLLECT(path) as ps
-      CALL apoc.convert.toTree(ps)
-      YIELD value
-      RETURN apoc.convert.toJson(value) AS output
-  `,{rootId: event.rootId});
-  return result.records.map(record => record.get('output'));
+    const checkRelation = await session.executeRead(async tx => {
+      const result = await tx.run(`
+        MATCH (a) WHERE ANY(k IN ['teamId', 'projectId', 'EmployeeCode'] WHERE toString(a[k]) CONTAINS $rootId)
+        MATCH (a)<-[r]-() WHERE type(r) CONTAINS $relN
+        RETURN r
+      `,{rootId: event.rootId, relN: event.relationName});
+      return result.records.map(record => record.get('r'));
     });
-    const regex = generateRegex(event.relationName);
-    const resp =  tree.toString().replace(regex,`"children":`);
+    if(checkRelation.length > 1){
+      const tree = await session.executeRead(async tx => {
+      //   const result = await tx.run(`
+      //   MATCH (a) WHERE ANY(k IN ['teamId', 'projectId', 'EmployeeCode'] WHERE toString(a[k]) CONTAINS $rootId)
+      //   CALL {
+      //     MATCH p=(a)<-[:${view.relation}*]-()
+      //         WHERE apoc.coll.duplicates(NODES(p)) = []
+      //     WITH p ORDER BY length(p) DESC
+      //     WITH COLLECT(DISTINCT p) AS ps
+      //     RETURN ps
+      //   }
+      //   WITH ps
+      //   CALL apoc.convert.toTree(ps)
+      //   YIELD value
+      //   RETURN apoc.convert.toJson(value)
+      // `,{rootId: view.rootId});
+      // return result.records.map(record => record.get('apoc.convert.toJson(value)'))[0];
+      const result = await tx.run(`
+        MATCH (a) WHERE ANY(k IN ['teamId', 'projectId', 'EmployeeCode'] WHERE toString(a[k]) CONTAINS $rootId)
+        CALL apoc.path.expandConfig(a, {
+          relationshipFilter: "${event.relationName}<",
+            minLevel: 1,
+            maxLevel: -1,
+            uniqueness:'NODE_GLOBAL',
+            bfs:true
+        })
+        YIELD path WHERE all(rel in relationships(path) WHERE rel.isActive)
+        WITH COLLECT(path) as ps
+        CALL apoc.convert.toTree(ps)
+        YIELD value
+        RETURN apoc.convert.toJson(value) AS output
+      `,{rootId: event.rootId});
+        return result.records.map(record => record.get('output'));
+      });
+      const regex = generateRegex(event.relationName);
+      const resp =  tree.toString().replace(regex,`"children":`);
+      return successResponse('Hierarchy fetched successfully', resp);
+    }
+    const rootNode = await session.executeRead(async tx => {
+      const result = await tx.run(`
+        MATCH (a) WHERE ANY(k IN ['teamId', 'projectId', 'EmployeeCode'] WHERE toString(a[k]) CONTAINS $rootId)
+        RETURN apoc.convert.toJson(a) AS output
+      `,{rootId: event.rootId});
+      return result.records.map(record => record.get('output'));
+    });
+    const resp =  rootNode.toString();
     return successResponse('Hierarchy fetched successfully', resp);
   } catch (err) {
     errorLogger("fetchHierarchy ", err);
