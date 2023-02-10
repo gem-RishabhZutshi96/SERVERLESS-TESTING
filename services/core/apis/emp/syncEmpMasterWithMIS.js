c08d190b826a0f2d28786dc4ed56676f2b9fea41
gem-manish <manish.pandey@geminisolutions.com>
2023-02-10 17:30:01 +0530

    Added new field after MIS API changes

--- services/core/apis/emp/syncEmpMasterWithMIS.js
+++ services/core/apis/emp/syncEmpMasterWithMIS.js
@@ -20,10 +20,10 @@ export const syncEmpMasterWithMIS = async(event) => {
   try{
     devLogger("syncEmpMasterWithMIS", event, "event");
     let userToken =null;
-    let  buf;
-    let  data;
-    let  timestamp;
-    let  fileName;
+    // let buf;
+    // let data;
+    // let timestamp;
+    // let fileName;
     await makeDBConnection();
     userToken = getUserToken(event);
     let authQuery={
@@ -35,14 +35,13 @@ export const syncEmpMasterWithMIS = async(event) => {
       return auth;
     }
     let misData = await getDataService();
+    let allEmp = await employeeMasterModel.find({isActive: true});
     let createArray = [];
     let updateNode = [];
     let updateArray = [];
     let deleteArray = [];
     misData.Result.forEach(async emp => {
-      let element = await employeeMasterModel.find({
-        'EmailId': {'$regex': `^${emp.EmailId}$`, $options: 'i'}
-      });
+      let element = allEmp.filter(el => (generateRegex(emp.EmailId).test(el.EmailId)));
       if(element.length < 1){
         createArray.push({
           "EmailId": emp["EmailId"] ? emp["EmailId"] : "",
@@ -55,64 +54,83 @@ export const syncEmpMasterWithMIS = async(event) => {
           "Location": emp["Location"] ? emp["Location"] : "",
           "ImagePath": emp["ImagePath"] ? emp["ImagePath"] : "",
           "MobileNumber": emp["MobileNumber"] ? emp["MobileNumber"] : "",
-          "Experience": emp["Experience"] ?emp["Experience"] : "",
+          "Experience": emp["Experience"] ? emp["Experience"] : "",
+          "ECTech": emp["ECTech"] ? emp["ECTech"] : "",
+          "DCTech": emp["DCTech"] ? emp["DCTech"] : "",
+          "isActive": true,
+          "createdAt": moment().format(),
+          "createdBy": auth.userEmail,
+          "updatedAt": "",
+          "updatedBy": "",
         });
       }
       if(element.length >= 1
-          && (emp.EmployeeCode != element[0].EmployeeCode ||
-          emp.EmployeeName != element[0].EmployeeName ||
-          emp.Designation != element[0].Designation ||
-          emp.ManagerCode != element[0].ManagerCode ||
-          emp.ImagePath != element[0].ImagePath))
-          {
-            updateNode.push({
-              "EmployeeCode": emp["EmployeeCode"] ? emp["EmployeeCode"] : "",
-              "EmployeeName": emp["EmployeeName"] ? emp["EmployeeName"] : "",
-              "Designation": emp["Designation"] ? emp["Designation"] : "",
-              "ManagerCode": emp["ManagerCode"] ? emp["ManagerCode"] : "",
-              "ImagePath": emp["ImagePath"] ? emp["ImagePath"] : "",
-          });
-        }
+        && (emp.EmployeeCode != element[0].EmployeeCode ||
+        emp.EmployeeName != element[0].EmployeeName ||
+        emp.Designation != element[0].Designation ||
+        emp.ManagerCode != element[0].ManagerCode ||
+        emp.ImagePath != element[0].ImagePath ||
+        emp.ECTech != element[0].ECTech ||
+        emp.DCTech != element[0].DCTech)) {
+        updateNode.push({
+          "EmployeeCode": emp["EmployeeCode"] ? emp["EmployeeCode"] : "",
+          "EmployeeName": emp["EmployeeName"] ? emp["EmployeeName"] : "",
+          "Designation": emp["Designation"] ? emp["Designation"] : "",
+          "ManagerCode": emp["ManagerCode"] ? emp["ManagerCode"] : "",
+          "ImagePath": emp["ImagePath"] ? emp["ImagePath"] : "",
+          "ECTech": emp["ECTech"] ? emp["ECTech"] : "",
+          "DCTech": emp["DCTech"] ? emp["DCTech"] : "",
+          "isActive": true,
+          "updatedAt": moment().format(),
+          "updatedBy": auth.userEmail,
+        });
+      }
       if(element.length >= 1
-          && (emp.EmployeeCode != element[0].EmployeeCode ||
-          emp.EmployeeName != element[0].EmployeeName ||
-          emp.DepartmentName != element[0].DepartmentName ||
-          emp.Designation != element[0].Designation ||
-          emp.ReportingManager != element[0].ReportingManager ||
-          emp.ManagerCode != element[0].ManagerCode ||
-          emp.Location != element[0].Location ||
-          emp.ImagePath != element[0].ImagePath ||
-          emp.MobileNumber != element[0].MobileNumber ||
-          emp.Experience != element[0].Experience))
-    {
-      updateArray.push({
-        "EmailId": emp["EmailId"] ? emp["EmailId"] : "",
-        "EmployeeCode": emp["EmployeeCode"] ? emp["EmployeeCode"] : "",
-        "EmployeeName": emp["EmployeeName"] ? emp["EmployeeName"] : "",
-        "DepartmentName": emp["DepartmentName"] ? emp["DepartmentName"] : "",
-        "Designation": emp["Designation"] ? emp["Designation"] : "",
-        "ReportingManager": emp["ReportingManager"] ? emp["ReportingManager"] : "",
-        "ManagerCode": emp["ManagerCode"] ? emp["ManagerCode"] : "",
-        "Location": emp["Location"] ? emp["Location"] : "",
-        "ImagePath": emp["ImagePath"] ? emp["ImagePath"] : "",
-        "MobileNumber": emp["MobileNumber"] ? emp["MobileNumber"] : "",
-        "Experience": emp["Experience"] ? emp["Experience"] : "",
+        && (emp.EmployeeCode != element[0].EmployeeCode ||
+        emp.EmployeeName != element[0].EmployeeName ||
+        emp.DepartmentName != element[0].DepartmentName ||
+        emp.Designation != element[0].Designation ||
+        emp.ReportingManager != element[0].ReportingManager ||
+        emp.ManagerCode != element[0].ManagerCode ||
+        emp.Location != element[0].Location ||
+        emp.ImagePath != element[0].ImagePath ||
+        emp.MobileNumber != element[0].MobileNumber ||
+        emp.Experience != element[0].Experience ||
+        emp.ECTech != element[0].ECTech ||
+        emp.DCTech != element[0].DCTech)) {
+        updateArray.push({
+          "EmailId": emp["EmailId"] ? emp["EmailId"] : "",
+          "EmployeeCode": emp["EmployeeCode"] ? emp["EmployeeCode"] : "",
+          "EmployeeName": emp["EmployeeName"] ? emp["EmployeeName"] : "",
+          "DepartmentName": emp["DepartmentName"] ? emp["DepartmentName"] : "",
+          "Designation": emp["Designation"] ? emp["Designation"] : "",
+          "ReportingManager": emp["ReportingManager"] ? emp["ReportingManager"] : "",
+          "ManagerCode": emp["ManagerCode"] ? emp["ManagerCode"] : "",
+          "Location": emp["Location"] ? emp["Location"] : "",
+          "ImagePath": emp["ImagePath"] ? emp["ImagePath"] : "",
+          "MobileNumber": emp["MobileNumber"] ? emp["MobileNumber"] : "",
+          "Experience": emp["Experience"] ? emp["Experience"] : "",
+          "ECTech": emp["ECTech"] ? emp["ECTech"] : "",
+          "DCTech": emp["DCTech"] ? emp["DCTech"] : "",
+          "isActive": true,
+          "updatedAt": moment().format(),
+          "updatedBy": auth.userEmail,
+        });
+      }
     });
-  }
-    });
-    const res = await employeeMasterModel.find();
-    res.forEach(document => {
+    allEmp.forEach(document => {
       let obj = misData.Result.find(emp => emp.EmailId == document.EmailId);
       if(obj == undefined){
         deleteArray.push(document.EmailId);
       }
     });
     if(createArray.length >= 1){
-      const bulk = employeeMasterModel.collection.initializeOrderedBulkOp();
+      // const bulk = employeeMasterModel.collection.initializeOrderedBulkOp();
       createArray.forEach(async emp => {
-        await bulk.insert(emp);
+        await employeeMasterModel.create(emp);
       });
-      await bulk.execute();
+      // await bulk.execute();
+      // const resp = await employeeMasterModel.insertMany(createArray);
     }
     if(updateArray.length >= 1){
       updateArray.forEach(async emp => {
@@ -130,62 +148,81 @@ export const syncEmpMasterWithMIS = async(event) => {
             ImagePath: emp.ImagePath,
             MobileNumber: emp.MobileNumber,
             Experience: emp.Experience,
+            ECTech: emp.ECTech,
+            DCTech: emp.DCTech,
+            isActive: emp.isActive,
+            updatedAt: emp.updatedAt,
+            updatedBy: emp.updatedBy
           },
         };
         await employeeMasterModel.updateMany(filter, updateDoc, options);
       });
     }
     if(createArray.length >= 1 || updateNode.length >= 1){
-      buf = Buffer.from(JSON.stringify(
-        {
-          'createNode': createArray,
-          'updateNode': updateNode
-        }
-      ));
-      timestamp = moment().format('DD-MM-YYYY_HH:mm:ss');
-      fileName = `json/${timestamp}--createOrUpdateNode.json`;
-      data = {
-        Bucket: parameterStore[process.env.stage].s3Params.sowBucket,
-        Key: fileName,
-        ContentType: 'application/json',
-        Body: buf
-      };
-      console.log("---- UPLODAING TO S3 ----");
-      await s3.upload(data).promise();
-      console.log("---- GETTING SIGNED URL FROM S3 ----");
-      let downloadURL = s3.getSignedUrl("getObject",{
-        Bucket: parameterStore[process.env.stage].s3Params.sowBucket,
-        Key: fileName,
-        Expires: 3600
-      });
+      // buf = Buffer.from(JSON.stringify(
+      //   {
+      //     'createNode': createArray,
+      //     'updateNode': updateNode
+      //   }
+      // ));
+      // timestamp = moment().format('DD-MM-YYYY_HH:mm:ss');
+      // fileName = `json/${timestamp}--createOrUpdateNode.json`;
+      // data = {
+      //   Bucket: parameterStore[process.env.stage].s3Params.sowBucket,
+      //   Key: fileName,
+      //   ContentType: 'application/json',
+      //   Body: buf
+      // };
+      // console.log("---- UPLODAING TO S3 ----");
+      // await s3.upload(data).promise();
+      // console.log("---- GETTING SIGNED URL FROM S3 ----");
+      // let downloadURL = s3.getSignedUrl("getObject",{
+      //   Bucket: parameterStore[process.env.stage].s3Params.sowBucket,
+      //   Key: fileName,
+      //   Expires: 3600
+      // });
+      // await main({
+      //   actionType: 'createOrUpdateEmpNeo4j',
+      //   s3JsonUrl: downloadURL
+      // });
       await main({
         actionType: 'createOrUpdateEmpNeo4j',
-        s3JsonUrl: downloadURL
+        createArray: createArray,
+        updateNode: updateNode
       });
     }
     if(deleteArray.length >= 1) {
-      await employeeMasterModel.remove({ EmailId: { $in: deleteArray } });
-      buf = Buffer.from(JSON.stringify(
-        {'deleteNode': deleteArray}));
-      timestamp = moment().format('DD-MM-YYYY_HH:mm:ss');
-      fileName = `json/${timestamp}--deleteNode.json`;
-      data = {
-        Bucket: parameterStore[process.env.stage].s3Params.sowBucket,
-        Key: fileName,
-        ContentType: 'application/json',
-        Body: buf
-      };
-      console.log("---- UPLODAING TO S3 ----");
-      await s3.upload(data).promise();
-      console.log("---- GETTING SIGNED URL FROM S3 ----");
-      let downloadURL = s3.getSignedUrl("getObject",{
-        Bucket: parameterStore[process.env.stage].s3Params.sowBucket,
-        Key: fileName,
-        Expires: 3600
-      });
+      await employeeMasterModel.remove.findOneAndUpdate(
+        { EmailId: { $in: deleteArray } },
+        { $set: { 'isActive' : false, 'updatedAt': moment().format(), 'updatedBy': auth.userEmail } },
+        {upsert: false}
+      );
+      // await employeeMasterModel.remove({ EmailId: { $in: deleteArray } });
+      // buf = Buffer.from(JSON.stringify(
+      //   {'deleteNode': deleteArray}));
+      // timestamp = moment().format('DD-MM-YYYY_HH:mm:ss');
+      // fileName = `json/${timestamp}--deleteNode.json`;
+      // data = {
+      //   Bucket: parameterStore[process.env.stage].s3Params.sowBucket,
+      //   Key: fileName,
+      //   ContentType: 'application/json',
+      //   Body: buf
+      // };
+      // console.log("---- UPLODAING TO S3 ----");
+      // await s3.upload(data).promise();
+      // console.log("---- GETTING SIGNED URL FROM S3 ----");
+      // let downloadURL = s3.getSignedUrl("getObject",{
+      //   Bucket: parameterStore[process.env.stage].s3Params.sowBucket,
+      //   Key: fileName,
+      //   Expires: 3600
+      // });
+      // await main({
+      //   actionType: 'deleteEmpNeo4j',
+      //   deleteUrl: downloadURL
+      // });
       await main({
-        actionType: 'deleteEmpNeo4j',
-        deleteUrl: downloadURL
+        actionType: 'createOrUpdateEmpNeo4j',
+        deleteArray: deleteArray
       });
     }
     return successResponse('Employee Master Table and Neo4J DB Synced Successfully with MIS');
@@ -194,3 +231,6 @@ export const syncEmpMasterWithMIS = async(event) => {
     return internalServer(`Error in DB`);
   }
 };
+function generateRegex(str) {
+  return new RegExp(`${str}`,"i");
+}
