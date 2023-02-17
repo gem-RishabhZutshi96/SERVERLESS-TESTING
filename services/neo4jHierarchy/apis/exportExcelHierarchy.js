1df22d12e3be01e0560bdaeda0b4a3da692c29e9
gem-manish <manish.pandey@geminisolutions.com>
2023-02-17 12:58:04 +0530

    Updated Imports and function response

--- services/neo4jHierarchy/apis/exportExcelHierarchy.js
+++ services/neo4jHierarchy/apis/exportExcelHierarchy.js
@@ -1,12 +1,16 @@
-import { internalServer, forbiddenRequest, successResponse } from "../../utilities/response/index";
+import { internalServer, forbiddenRequest, badRequest } from "../../utilities/response/index";
 import { accessAllowed } from "../../utilities/validateToken/authorizer";
 import { getUserToken } from "../../utilities/validateToken/getUserToken";
 import { devLogger, errorLogger } from "../utils/log-helper";
+import { exportHierarchyExcel } from "../utils/exportExcel";
+import { makeDBConnection } from "../../utilities/db/mongo";
+import { viewModel } from "../../utilities/dbModels/view";
 import moment from 'moment';
 export const exportExcelHierarchy = async (event) => {
     try {
         devLogger("exportExcelHierarchy", event, "event");
         let userToken = null;
+        await makeDBConnection();
         userToken = getUserToken(event);
         let authQuery = {
             token: userToken,
@@ -16,9 +20,15 @@ export const exportExcelHierarchy = async (event) => {
         if ( auth.access !== "allowed") {
             return forbiddenRequest("❌❌  User is not allowed to access the data");
         }
-        let timestamp = moment().format('DD-MM-YYYY_HH:mm:ss');
-        const excelFilePath = await exportExcelHierarchy({'fileName': 'excels' + '/' + timestamp + '--' +`ProjectMasterTable.xlsx`});
-        return successResponse("👍👍Excel Exported Successfully", excelFilePath);
+        let source = event.path.source || event.pathParameters.source;
+        const sourceViews = await viewModel.find({ 'name': { '$regex': source, '$options': 'i' } });
+        if(sourceViews.length >= 1){
+            let timestamp = moment().format('DD-MM-YYYY_HH:mm:ss');
+            const resp = await exportHierarchyExcel({'fileName': 'hierarchyExcels' + '/' + timestamp + '--' +`${sourceViews[0].name}.xlsx`, 'relationName': sourceViews[0].relationName});
+            return resp;
+        } else {
+            return badRequest("Invalid path parameters");
+        }
     } catch (err) {
         errorLogger("exportExcelHierarchy", err, "Error db call");
         return internalServer(`Error in DB `);
