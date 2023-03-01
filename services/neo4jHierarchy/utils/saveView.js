ab0f94e998ef7f256257469195b376909a66dd44
gem-manish <manish.pandey@geminisolutions.com>
2023-03-01 16:04:18 +0530

    Removed moment library

--- services/neo4jHierarchy/utils/saveView.js
+++ services/neo4jHierarchy/utils/saveView.js
@@ -4,7 +4,6 @@ import { main } from "../neo4j-handler";
 import { parameterStore } from "../../utilities/config/commonData";
 import { makeDBConnection } from "../../utilities/db/mongo";
 import { viewModel } from "../../utilities/dbModels/view";
-import moment from 'moment';
 import AWS from 'aws-sdk';
 const s3 = new AWS.S3();
 s3.config.update({
@@ -28,24 +27,25 @@ export const saveViewToS3 = async (event) => {
             let buf = Buffer.from(JSON.stringify(
                 {
                     'viewName': sourceViews[0].name,
-                    'type': sourceViews[0].type,
+                    'rootType': sourceViews[0].type,
                     'viewId': sourceViews[0].viewId,
                     'relationName': sourceViews[0].relationName,
                     'rootId': sourceViews[0].rootId,
                     'hierachyData': resp.data,
                 }
             ));
-            let timestamp = moment().format('DD-MM-YYYY_HH:mm:ss');
-            let fileName = 'hierarchyJSON' + '/' + sourceViews[0].name + '__' + timestamp + '.json';
+            let timestamp = new Date();
+            timestamp=timestamp.toISOString();
+            let fileName = `backups/hierarchy/${sourceViews[0].viewId}__${timestamp}.json`;
             let params = {
-                Bucket: parameterStore[process.env.stage].s3Params.sowBucket,
+                Bucket: parameterStore[process.env.stage].s3Params.orgchartS3Bucket,
                 Key: fileName,
                 ContentType: 'application/json',
                 Body: buf
             };
             let responseOfUpload;
             console.log("---- UPLODAING TO S3 ----");
-            const upload = new Promise((resolve, reject) => {
+            () => new Promise((resolve, reject) => {
                 s3.upload(params, function (err, data) {
                   if (err) {
                     console.log(err, err.stack);
@@ -67,11 +67,11 @@ export const saveViewToS3 = async (event) => {
             });
             // console.log("---- GETTING SIGNED URL FROM S3 ----");
             // let downloadURL = s3.getSignedUrl("getObject",{
-            //     Bucket: parameterStore[process.env.stage].s3Params.sowBucket,
+            //     Bucket: parameterStore[process.env.stage].s3Params.orgchartS3Bucket,
             //     Key: fileName,
             //     Expires: 3600
             // });
-            return successResponse("Current view hierarchy uploded successfully to S3", upload);
+            return successResponse("Current view hierarchy uploded successfully to S3");
         }
         return badRequest("Invalid relation name");
     } catch (err) {
